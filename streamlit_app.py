5dc45aa651f9426783f40bb6668c9330596dbc30
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-04-28 14:20:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('🤖 This is the Machine Learning App')
 
 st.write('This is the app build a machine learning model')
 with st.expander('Data'):
-  st.write("**Raw Data**)
+  st.write("**Raw Data**")
   df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
   df
 
