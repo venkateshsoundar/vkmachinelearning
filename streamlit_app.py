044124a1745015d90fd7ec53b1e383264b056489
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-04-28 15:01:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,10 @@ import pandas as pd
 st.title('🤖 This is the Machine Learning App')
 
 st.write('This is the app build a machine learning model')
+df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
+
 with st.expander('Data'):
   st.write("**Raw Data**")
-  df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
   df
 
   st.write("**X**")
