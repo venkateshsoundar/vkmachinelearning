d32e1578dc8c4b31989f199de3bbcef9d53db9aa
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-04-29 12:51:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,6 @@ from openai import OpenAI
 
 
 st.title('🐧 Penguin Dataset Chatbot')
-# --- Streamlit UI ---
-st.set_page_config(page_title="Penguin QA Chatbot")
-
 st.write('This is the app build a machine learning model')
 df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
 
