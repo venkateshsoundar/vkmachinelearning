50f490dbf173ee4958f5df48626d78682e3cfffb
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-04-28 19:53:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,5 +80,6 @@ clf.fit(X,y)
 #Apply prediction
 predicition=clf.predict(inputrow)
 prediction_proba=clf.predict_proba(inputrow)
-prediction_proba.rename(columns={0:'Adelie',1:'Chinstrap',2:'Gentoo'})
+prediction_proba_df = pd.DataFrame(prediction_proba, columns=clf.classes_)
+prediction_proba_df.rename(columns={0: 'Adelie', 1: 'Chinstrap', 2: 'Gentoo'}, inplace=True)
 prediction_proba
