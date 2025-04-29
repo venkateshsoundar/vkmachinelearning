3032de8fafbaa851cb07be94a614028fb8eac890
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-04-28 20:08:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,9 +92,24 @@ with st.expander('Predicted_Species'):
     column_config={
         "Adelie": st.column_config.ProgressColumn(
             "Adelie",
-            format="$%f",
+            format="%f",
+            width='medium'
             min_value=0,
-            max_value=1000,
+            max_value=1,
+        ),
+        "Chinstrap": st.column_config.ProgressColumn(
+            "Chinstrap",
+            format="%f",
+            width='medium'
+            min_value=0,
+            max_value=1,
+        ),
+        "Gentoo": st.column_config.ProgressColumn(
+            "Gentoo",
+            format="%f",
+            width='medium'
+            min_value=0,
+            max_value=1,
         ),
     },
     hide_index=True,
