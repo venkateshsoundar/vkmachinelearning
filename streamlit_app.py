d9cf2d4d5843b4cb693c0543d6a69443f30b30d6
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-04-28 19:52:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,5 +80,5 @@ clf.fit(X,y)
 #Apply prediction
 predicition=clf.predict(inputrow)
 prediction_proba=clf.predict_proba(inputrow)
-prediction_proba.rename(columns={0:'Adelie',1:'Chinstrap',2:'Gentoo'}
+prediction_proba.rename(columns={0:'Adelie',1:'Chinstrap',2:'Gentoo'})
 prediction_proba
