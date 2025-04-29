6f12c0628af32ed665ccdef49ff018b963b7c3fc
Venkatesh Soundararajan <114813214+venkateshsoundar@users.noreply.github.com>
2025-04-29 12:50:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ from openai import OpenAI
 
 
 st.title('🐧 Penguin Dataset Chatbot')
+# --- Streamlit UI ---
+st.set_page_config(page_title="Penguin QA Chatbot")
 
 st.write('This is the app build a machine learning model')
 df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
@@ -17,8 +19,7 @@ client = OpenAI(
     api_key=api_key,
 )
 
-# --- Streamlit UI ---
-st.set_page_config(page_title="Penguin QA Chatbot")
+
 
 if "history" not in st.session_state:
     st.session_state.history = []
